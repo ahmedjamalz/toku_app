f10c851677804de1bc36dd882ec9b1cf96df6664
ahmedjamalz <ajamalzayed@gmail.com>
2024-03-11 16:45:57 +0200

    2

--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -6,9 +6,43 @@ class Numbers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Numbers'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Color(0xFF453025),
+          title: Text('Numbers'),
+        ),
+        body: Container(
+          color: Color(0xFFF9942E),
+          height: 100,
+          child: Row(
+            children: [
+              Container(
+                  color: Color(0xFFFFF4DB),
+                  child: Image.asset('assets/images/numbers/number_one.png')),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'ichi',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                    Text(
+                      'one',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(
+                flex: 1,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 13),
+                child: Icon(Icons.play_arrow, color: Colors.white),
+              )
+            ],
+          ),
+        ));
   }
 }
